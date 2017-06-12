2616b8aa8bd188e478236224781ad52a23487b71
Setyabudi Dwisandi Arifin <setyabudiarifin@gmail.com>
2017-06-12 23:03:22 +0700

    first java commit

---
+++ src/main/java/com/setyabudi/arifin/controllers/indexController.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author Setyabudi
+ */
+package com.setyabudi.arifin.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class indexController {
+
+    @RequestMapping(value = "/*", method = RequestMethod.GET)
+    public ModelAndView printHello() {
+        ModelAndView model = new ModelAndView("index");
+        model.addObject("message", "hello world");
+        return model;
+    }
+
+    @RequestMapping(value = "/ahay", method = RequestMethod.GET)
+    public String printAhay(ModelMap model) {
+        model.addAttribute("message", "ahay!");
+        return "hello";
+    }
+
+}
